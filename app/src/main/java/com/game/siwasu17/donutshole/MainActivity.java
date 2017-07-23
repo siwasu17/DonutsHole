69494603dc21dc600040b2bfca8052349130566d
siwasu17 <siwasu17@gmail.com>
2017-07-23 22:38:53 +0900

    Scroll and reload.

--- app/src/main/java/com/game/siwasu17/donutshole/MainActivity.java
+++ app/src/main/java/com/game/siwasu17/donutshole/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.widget.AbsListView;
 import android.widget.Button;
 import android.widget.GridView;
 import android.widget.ImageView;
@@ -15,6 +16,7 @@ import com.game.siwasu17.donutshole.models.ImageEntry;
 import com.game.siwasu17.donutshole.services.TiqavService;
 import com.squareup.picasso.Picasso;
 
+import java.text.MessageFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -30,6 +32,7 @@ public class MainActivity extends AppCompatActivity {
 
     private Button mCallButton;
     private GridView mGridView;
+    private List<ImageEntry> mImageEntryList = new ArrayList<>();
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -64,7 +67,35 @@ public class MainActivity extends AppCompatActivity {
 
         // グリッドビュー
         mGridView = (GridView) findViewById(R.id.gridview);
+        mGridView.setOnScrollListener(new AbsListView.OnScrollListener() {
+            private boolean loading = true;
 
+            @Override
+            public void onScrollStateChanged(AbsListView absListView, int i) {
+
+            }
+
+            @Override
+            public void onScroll(AbsListView absListView, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
+                System.out.println(
+                        MessageFormat.format("FirstVisPos: {0}, VisibleItemCount: {1}, total: {2}",
+                                firstVisibleItem, visibleItemCount, totalItemCount)
+                );
+
+
+                if (totalItemCount == (firstVisibleItem + visibleItemCount)) {
+                    if(!loading) {
+                        //ロード中でなければロード
+                        System.out.println("Load!");
+                        loading = true;
+                        callTiqavService();
+                    }
+                }else{
+                    loading = false;
+                }
+
+            }
+        });
         //画像のロード
         callTiqavService();
     }
@@ -80,7 +111,8 @@ public class MainActivity extends AppCompatActivity {
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(imgs -> {
                             System.out.println(imgs);
-                            mGridView.setAdapter(new HueAdapter(this, Arrays.asList(imgs)));
+                            mImageEntryList.addAll(Arrays.asList(imgs));
+                            mGridView.setAdapter(new HueAdapter(this, mImageEntryList));
                             mGridView.invalidate();
                         }
                         , Throwable::printStackTrace
