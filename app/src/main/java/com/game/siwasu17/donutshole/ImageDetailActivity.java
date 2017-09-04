ba3ef33672e18127c346f1553614c10e7f80bfe5
siwasu17 <siwasu17@gmail.com>
2017-09-05 00:57:38 +0900

    Simply share image url.

--- app/src/main/java/com/game/siwasu17/donutshole/ImageDetailActivity.java
+++ app/src/main/java/com/game/siwasu17/donutshole/ImageDetailActivity.java
@@ -1,8 +1,11 @@
 package com.game.siwasu17.donutshole;
 
 import android.app.ActionBar;
+import android.support.v4.app.ShareCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import com.game.siwasu17.donutshole.models.ImageEntry;
@@ -26,6 +29,30 @@ public class ImageDetailActivity extends AppCompatActivity {
                 .fit()
                 .centerInside()
                 .into(imageView);
+
+        // builderの生成　ShareCompat.IntentBuilder.from(Context context);
+        ShareCompat.IntentBuilder builder = ShareCompat.IntentBuilder.from(this);
+
+        Button shareButton = (Button)findViewById(R.id.share_button);
+        shareButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                // アプリ一覧が表示されるDialogのタイトルの設定
+                builder.setChooserTitle("アプリを選択");
+
+                // シェアするタイトル
+                builder.setSubject("Image from tiqav");
+
+                // シェアするテキスト
+                builder.setText(imageUrl);
+
+                // シェアするタイプ
+                builder.setType("text/plain");
+
+                // Shareアプリ一覧のDialogの表示
+                builder.startChooser();
+            }
+        });
 
     }
 }
