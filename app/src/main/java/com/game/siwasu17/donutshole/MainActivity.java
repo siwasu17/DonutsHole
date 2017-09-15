4ff41e3fa194d31ba384acff6c484cf694e1b444
siwasu17 <siwasu17@gmail.com>
2017-09-16 00:01:15 +0900

    Fix search page bug.

--- app/src/main/java/com/game/siwasu17/donutshole/MainActivity.java
+++ app/src/main/java/com/game/siwasu17/donutshole/MainActivity.java
@@ -1,32 +1,19 @@
 package com.game.siwasu17.donutshole;
 
-import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
-import android.graphics.Color;
 import android.graphics.drawable.Drawable;
-import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.annotation.RequiresApi;
 import android.support.design.widget.BottomNavigationView;
-import android.support.v4.content.FileProvider;
 import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.ActionBar;
-import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.SearchView;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.AbsListView;
-import android.widget.AdapterView;
-import android.widget.Button;
 import android.widget.GridView;
-import android.widget.ImageView;
-import android.widget.ListAdapter;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.game.siwasu17.donutshole.models.ImageEntry;
@@ -35,14 +22,12 @@ import com.squareup.picasso.Picasso;
 import com.squareup.picasso.Target;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.text.MessageFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Optional;
 
 import io.reactivex.Observable;
 import io.reactivex.android.schedulers.AndroidSchedulers;
@@ -55,8 +40,9 @@ public class MainActivity extends AppCompatActivity {
     private List<ImageEntry> mImageEntryList = new ArrayList<>();
 
     SearchView mSearchView;
+
+    boolean searchWordChanged = false;
     //検索ワード
-    //String searchWord = null;
     String searchWord;
 
     public static final String IMAGE_CACHE_KEY = "IMAGE_CACHE_KEY";
@@ -176,18 +162,24 @@ public class MainActivity extends AppCompatActivity {
      * TiqavのAPIを叩いてGridViewに画像を反映させる
      */
     private void callTiqavService() {
-        Toast.makeText(this, "Loading...", Toast.LENGTH_SHORT).show();
-
         //Interfaceから実装を取得
         TiqavService tiqavService = ServiceFactory.createTiqavService();
         Observable<ImageEntry[]> apiCall;
         if(null != searchWord && !searchWord.isEmpty()) {
-            mImageEntryList.clear();
-            //FIXME: 無限スクロールしていくと困る
-            apiCall = tiqavService.search(searchWord);
+            if(searchWordChanged) {
+                mImageEntryList.clear();
+                mGridView.setAdapter(null);
+                apiCall = tiqavService.search(searchWord);
+                searchWordChanged = false;
+            }else{
+                //do nothing
+                return;
+            }
         }else{
             apiCall = tiqavService.searchRandom();
         }
+
+        Toast.makeText(this, "Loading...", Toast.LENGTH_SHORT).show();
 
         apiCall.subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
@@ -252,6 +244,7 @@ public class MainActivity extends AppCompatActivity {
         if (searchWord != null && !searchWord.equals("")) {
             // searchWordがあることを確認
             this.searchWord = searchWord;
+            this.searchWordChanged = true;
         }
         // 虫眼鏡アイコンを隠す
         mSearchView.setIconified(false);
